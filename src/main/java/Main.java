81c53fe157011db4ce25021bd1be3f4fa0625990
vikramv911 <vikram.venkatesan@kellogg.ox.ac.uk>
2017-01-19 11:12:13 +0000

    added tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,65 +13,70 @@ import com.heroku.sdk.jdbc.DatabaseUrl;
 
 public class Main {
   public static void main(String... args) throws Exception {
-    RatpackServer.start(s -> s
-        .serverConfig(c -> c
-          .baseDir(BaseDir.find())
-          .env())
-
-        .registry(Guice.registry(b -> b
-          .module(TextTemplateModule.class, conf -> conf.setStaticallyCompile(true))))
-
-        .handlers(chain -> chain
-            .get(ctx -> ctx.render(groovyTemplate("index.html")))
-
-            .get("hello", ctx -> {
-              ctx.render("Hello!");
-            })
-
-                .get("testing", ctx -> {
-                    ctx.render("look at something else!");
-                })
-
-                .get("vik1", ctx -> {
-                    ctx.render("vikram!");
-                })
-
-            .get("db", ctx -> {
-              boolean local = !"cedar-14".equals(System.getenv("STACK"));
-
-              Blocking.get(() -> {
-                Connection connection = null;
-
-                try {
-                  connection = DatabaseUrl.extract(local).getConnection();
-                  Statement stmt = connection.createStatement();
-                  stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-                  stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-                  return stmt.executeQuery("SELECT tick FROM ticks");
-                } finally {
-                  if (connection != null) try {
-                    connection.close();
-                  } catch (SQLException e) {
-                  }
-                }
-              }).onError(throwable -> {
-                Map<String, Object> attributes = new HashMap<>();
-                attributes.put("message", "There was an error: " + throwable);
-                ctx.render(groovyTemplate(attributes, "error.html"));
-              }).then(rs -> {
-                ArrayList<String> output = new ArrayList<>();
-                while (rs.next()) {
-                  output.add("Read from DB: " + rs.getTimestamp("tick"));
-                }
-
-                Map<String, Object> attributes = new HashMap<>();
-                attributes.put("results", output);
-                ctx.render(groovyTemplate(attributes, "db.html"));
-              });
-            })
-
-            .files(f -> f.dir("public"))
-        )
-    );
+      startServer();
   }
+
+    public static void startServer() throws Exception {
+        RatpackServer.start(s -> s
+            .serverConfig(c -> c
+              .baseDir(BaseDir.find())
+              .env())
+
+            .registry(Guice.registry(b -> b
+              .module(TextTemplateModule.class, conf -> conf.setStaticallyCompile(true))))
+
+            .handlers(chain -> chain
+                .get(ctx -> ctx.render(groovyTemplate("index.html")))
+
+                .get("hello", ctx -> {
+                  ctx.render("Hello!");
+                })
+
+                    .get("testing", ctx -> {
+                        ctx.render("look at something else!");
+                    })
+
+                    .get("vik1", ctx -> {
+                        ctx.render("vikram!");
+                    })
+
+
+                .get("db", ctx -> {
+                  boolean local = !"cedar-14".equals(System.getenv("STACK"));
+
+                  Blocking.get(() -> {
+                    Connection connection = null;
+
+                    try {
+                      connection = DatabaseUrl.extract(local).getConnection();
+                      Statement stmt = connection.createStatement();
+                      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+                      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+                      return stmt.executeQuery("SELECT tick FROM ticks");
+                    } finally {
+                      if (connection != null) try {
+                        connection.close();
+                      } catch (SQLException e) {
+                      }
+                    }
+                  }).onError(throwable -> {
+                    Map<String, Object> attributes = new HashMap<>();
+                    attributes.put("message", "There was an error: " + throwable);
+                    ctx.render(groovyTemplate(attributes, "error.html"));
+                  }).then(rs -> {
+                    ArrayList<String> output = new ArrayList<>();
+                    while (rs.next()) {
+                      output.add("Read from DB: " + rs.getTimestamp("tick"));
+                    }
+
+                    Map<String, Object> attributes = new HashMap<>();
+                    attributes.put("results", output);
+                    ctx.render(groovyTemplate(attributes, "db.html"));
+                  });
+                })
+
+                .files(f -> f.dir("public"))
+            )
+        );
+    }
 }
---
+++ src/main/java/MainTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Created by vikramv on 19/01/2017.
+ */
+public class MainTest {
+    @Test
+    public void startServer() throws Exception {
+    assertTrue(true);
+    }
+
+}
